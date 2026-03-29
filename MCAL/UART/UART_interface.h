f445b10a1a198d20636d86059e104d38fa6485e5
YoussefHussien-1 <youssefhussein1122@gmail.com>
2026-03-30 00:15:56 +0200

    GPS

--- MCAL/UART/UART_interface.h
+++ MCAL/UART/UART_interface.h
@@ -5,7 +5,7 @@
 
 void MUSART_voidInit(UART_t *UARTx, u32 Copy_u32BaudRate);
 void UART_voidTransmit(u8 Copy_u8Data );
-u8 UART_u8Receive(void);
+u8 UART_u8Receive(UART_t *UARTx);
 void MUSART1_voidSendString(u8 *Copy_ptrString);
 
 
