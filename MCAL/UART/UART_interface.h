400fb3728f522728db681b094478004bc96f7b56
YoussefHussien-1 <youssefhussein1122@gmail.com>
2026-03-29 23:18:49 +0200

    UART DRIVER

---
+++ MCAL/UART/UART_interface.h
@@ -0,0 +1,12 @@
+#ifndef         UART_INTERFACE_H_
+#define         UART_INTERFACE_H_
+
+
+
+void MUSART_voidInit(UART_t *UARTx, u32 Copy_u32BaudRate);
+void UART_voidTransmit(u8 Copy_u8Data );
+u8 UART_u8Receive(void);
+void MUSART1_voidSendString(u8 *Copy_ptrString);
+
+
+#endif
